31f1024b06b9c1efae2c8afbd6ab7646417537c6
Nathan Tannar <nathantannar4@gmail.com>
2025-04-09 20:14:04 -0700

    2.1.1

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/Engine", from: "2.0.4"),
+        .package(url: "https://github.com/nathantannar4/Engine", from: "2.0.5"),
     ],
     targets: [
         .target(
